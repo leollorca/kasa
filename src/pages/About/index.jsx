83a3871792c4f338928dfc20bbf7254d926d520f
leollorca <leollorca@proton.me>
2024-03-12 16:41:16 +0100

    fix styling issues

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -6,14 +6,24 @@ import Collapse from '../../components/Collapse';
 import banner from '../../images/about-banner.jpg';
 
 function About() {
+  const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+    "In lacinia maximus nibh ut commodo. Aenean ut nunc pharetra, dictum leo" +
+    "vel, interdum augue. Sed nec pulvinar lorem, vel tincidunt urna." +
+    "Mauris eget ultrices risus. Pellentesque purus nunc, porttitor a elit" +
+    "id, aliquam tempus urna. Donec viverra suscipit est. In egestas libero" +
+    "in ligula euismod, a sagittis ante hendrerit. In bibendum hendrerit" +
+    "eros, sed volutpat eros placerat a. Vivamus non interdum metus," +
+    "vitae ullamcorper nunc. Pellentesque eu sem tincidunt, pretium est" +
+    "nec, lobortis augue."
+
   return (
     <main>
       <PageBanner imgPath={banner} />
       <div className="about-collapses">
-        <Collapse name="Fiabilité" />
-        <Collapse name="Respect" />
-        <Collapse name="Service" />
-        <Collapse name="Sécurité" />
+        <Collapse name="Fiabilité" content={content} />
+        <Collapse name="Respect" content={content} />
+        <Collapse name="Service" content={content} />
+        <Collapse name="Sécurité" content={content} />
       </div>
     </main>
   );
